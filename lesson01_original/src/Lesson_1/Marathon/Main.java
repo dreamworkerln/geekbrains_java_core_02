888207cff8e7cfc97f6c5ba7b4e1dc334edf9212
hatuncev <hatuncev.ku@kvant-telecom.ru>
2019-03-02 17:42:27 +0300

    add Course, Team

--- lesson01_original/src/Lesson_1/Marathon/Main.java
+++ lesson01_original/src/Lesson_1/Marathon/Main.java
@@ -2,16 +2,29 @@ package Lesson_1.Marathon;
 
 public class Main {
     public static void main(String[] args) {
-        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
-        Obstacle[] course = {new Cross(80), new Wall(2), new Wall(1), new Cross(120)};
-        for (Competitor c : competitors) {
-            for (Obstacle o : course) {
-                o.doIt(c);
-                if (!c.isOnDistance()) break;
-            }
-        }
-        for (Competitor c : competitors) {
-            c.info();
-        }
+
+        // ------------------------------------
+
+        Team team = new Team("Dream team");
+        team.add(new Human("Боб"));
+        team.add(new Cat("Барсик"));
+        team.add(new Dog("Бобик"));
+
+        Course course = new Course();
+        course.add(new Cross(80));
+        course.add(new Water(20));
+        course.add(new Wall(2));
+
+
+        // ------------------------------------
+
+        course.doIt(team);
+
+        // ------------------------------------
+        System.out.println();
+        // ------------------------------------
+
+        // results
+        team.getAllInfo();
     }
 }
---
+++ lesson01_original/src/Lesson_1/Marathon/Team.java
@@ -0,0 +1,70 @@
+package Lesson_1.Marathon;
+
+/*
+2. Добавить класс Team, который будет содержать название команды,
+ массив из четырех участников (в конструкторе можно сразу указыватьвсех участников ),
+метод для вывода информации о членах команды, прошедших дистанцию,
+метод вывода информации обо всех членах команды.
+ */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Team implements Iterable<Competitor>{
+
+
+    private String name;
+    private List<Competitor> list = new ArrayList<>();
+    
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+
+    public void add(Competitor competitor) {
+
+        if (competitor == null)
+            throw new IllegalArgumentException("competitor == null");
+
+        list.add(competitor);
+    }
+
+
+
+    public void getActiveInfo() {
+
+        System.out.println("Team \"" + name +  "\" active members:");
+
+        for (Competitor c : list) {
+
+            if (c.isOnDistance())
+                c.info();
+        }
+
+    }
+
+
+    public void getAllInfo() {
+
+        System.out.println("Team \"" + name +  "\" members results:");
+
+        for (Competitor c : list) {
+            c.info();
+        }
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    // ---------------------------------------------------------------------
+
+    @Override
+    public Iterator<Competitor> iterator() {
+        return list.iterator();
+    }
+}
