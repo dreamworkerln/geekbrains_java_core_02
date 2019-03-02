fe8532d3a567c8ef1c9412d6df9321f24c6afead
hatuncev <hatuncev.ku@kvant-telecom.ru>
2019-03-02 12:01:17 +0300

    initial

---
+++ lesson01_additional/src/test/java/ru/home/geekbrains/java_core_02/AppTest.java
@@ -0,0 +1,20 @@
+package ru.home.geekbrains.java_core_02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
